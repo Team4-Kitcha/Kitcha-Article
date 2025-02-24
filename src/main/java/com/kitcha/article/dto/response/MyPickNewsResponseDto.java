ef8714ab7813101012fb23ceed55fac976c33717
SRASONY <leeseula0123@gmail.com>
2025-02-24 21:33:56 +0900

    Refactor : 응답 데이터 snake_case로 변경

--- src/main/java/com/kitcha/article/dto/response/MyPickNewsResponseDto.java
+++ src/main/java/com/kitcha/article/dto/response/MyPickNewsResponseDto.java
@@ -8,9 +8,19 @@ import lombok.Data;
 
 @AllArgsConstructor
 public class MyPickNewsResponseDto {
+
+    @JsonProperty("news_title")
     private String newsTitle;      // 기사 제목
+
+    @JsonProperty("news_date")
     private String newsDate;       // 기사 날짜
+
+    @JsonProperty("news_url")
     private String newsUrl;        // 기사 URL
+
+    @JsonProperty("short_summary")
     private String shortSummary;   // 짧은 요약
+
+    @JsonProperty("long_summary")
     private String longSummary;    // 상세 요약
 }
--- src/main/java/com/kitcha/article/dto/response/RandomNewsResponseDto.java
+++ src/main/java/com/kitcha/article/dto/response/RandomNewsResponseDto.java
@@ -1,13 +1,19 @@
 package com.kitcha.article.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class RandomNewsResponseDto {
+
+    @JsonProperty("news_title")
     private String newsTitle;
-    private String longSummary;    // 상세 요약
+
+    @JsonProperty("long_summary")
+    private String longSummary;
+
     private String interest;       // 관심사
     private String keyword;        // 핵심 키워드 (검색용)
 }
