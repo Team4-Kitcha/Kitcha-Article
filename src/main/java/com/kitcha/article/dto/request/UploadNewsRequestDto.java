6646a63ed8db18e3708cc719c859642870e87458
SRASONY <leeseula0123@gmail.com>
2025-02-24 20:51:26 +0900

    Refactor: Dto 정리

--- src/main/java/com/kitcha/article/dto/request/UploadNewsRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.kitcha.article.dto.request;
-
-import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-@Data
-public class UploadNewsRequestDto {
-    private MultipartFile imageFile;  // 업로드할 이미지 파일
-}
