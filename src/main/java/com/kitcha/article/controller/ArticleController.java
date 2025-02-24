92db8096cc37aa85f341479c5d1beebed8725877
SRASONY <leeseula0123@gmail.com>
2025-02-24 21:17:59 +0900

    feat: 게이트웨이 헤더 처리 기능 구현

--- src/main/java/com/kitcha/article/controller/ArticleController.java
+++ src/main/java/com/kitcha/article/controller/ArticleController.java
@@ -31,7 +31,9 @@ public class ArticleController {
 
     // MyPick 뉴스 가챠 API
     @GetMapping("/mypick")
-    public ResponseEntity<List<MyPickNewsResponseDto>> getMyPickNews(@RequestParam String keyword) {
+    public ResponseEntity<List<MyPickNewsResponseDto>> getMyPickNews(
+            @RequestParam String keyword,
+            @RequestHeader("X-User-ID") String userId) {
         // 뉴스 목록 가져오기
         List<MyPickNewsResponseDto> newsList = myPickNewsService.getMyPickNews(keyword);
         //result에 List 담기
@@ -42,14 +44,16 @@ public class ArticleController {
 
     // 랜덤 뉴스 가챠 API
     @GetMapping("/random")
-    public ResponseEntity<RandomNewsResponseDto> getRandomNews() {
+    public ResponseEntity<RandomNewsResponseDto> getRandomNews(@RequestHeader("X-User-ID") String userId) {
         RandomNewsResponseDto randomNews = randomNewsService.getRandomNews();
         return ResponseEntity.ok(randomNews);
     }
 
     // 관심사 및 키워드 기반 뉴스 조회 API
     @PostMapping("/interest_news")
-    public ResponseEntity<Map<String, Object>> getNewsByKeyword(@RequestBody InterestNewsRequestDto request) {
+    public ResponseEntity<Map<String, Object>> getNewsByKeyword(
+            @RequestBody InterestNewsRequestDto request,
+            @RequestHeader("X-User-ID") String userId) {
         String interest = request.getInterest();
         String keyword = request.getKeyword();
 
@@ -70,7 +74,9 @@ public class ArticleController {
 
     // 업로드 뉴스 가챠 API
     @PostMapping("/upload")
-    public ResponseEntity<List<MyPickNewsResponseDto>> uploadImageAndGetNews(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<List<MyPickNewsResponseDto>> uploadImageAndGetNews(
+            @RequestParam("file") MultipartFile file,
+            @RequestHeader("X-User-ID") String userId) {
         // 1. 이미지 처리 및 키워드 기반 뉴스 조회
         List<MyPickNewsResponseDto> newsList = uploadNewsService.processUploadedImage(file);
         // 2. 결과 반환
